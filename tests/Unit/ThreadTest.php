0b2bdb8d8d11c690fa4905d104d6af4d96899051
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2019-11-19 23:57:31 +0300

    Update ThreadTest

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\Notifications\ThreadWasUpdated;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
@@ -49,6 +51,22 @@ class ThreadTest extends TestCase
         ]);
 
         $this->assertCount(1, $this->thread->replies);
+    }
+
+    /** @test */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()
+            ->thread
+            ->subscribe()
+            ->addReply([
+            'user_id' => 1001,
+            'body' => 'Good Thread'
+        ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
     }
 
     /** @test */
