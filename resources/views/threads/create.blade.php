c189b38e93b8a4beceda929280990a0ad687a99e
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2019-12-31 18:32:13 +0300

    update create thread page

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -1,61 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Create a new Thread</div>
-                    <div class="card-body">
-                        <form method="POST" action="/threads">
-                            @csrf
-                            <div class="form-group">
-                                <label for="channel">Choose a Channel</label>
-                                <select name="channel_id" class="form-control @error('channel_id') is-invalid @enderror"
-                                        required>
-                                    <option value="">Choose One</option>
-                                    @foreach ($channels as $channel)
-                                        <option
-                                            value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
-                                            {{ $channel->name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('channel_id')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="title">Thread Title</label>
-                                <input type="text" name="title" id="title"
-                                       class="form-control @error('title') is-invalid @enderror"
-                                       value="{{ old('title') }}"
-                                       required
-                                >
-
-                                @error('title')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="body">Thread Body</label>
-                                <vue-trix  placeholder="Enter Your content here ..."></vue-trix>
-                                @error('body')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <button type="submit" class="btn btn-primary">Publish</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+<div class="font-sans">
+    <div class="container mx-auto">
+        <div class="mb-2">
+            <h1 class="text-2xl text-gray-700 font-medium uppercase tracking-wider">Create a new Thread</h1>
         </div>
-    </div>
+        <div class="">
+            <form method="POST" action="/threads" class="w-full bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                @csrf
+                <div class="flex flex-wrap -mx-3 mb-6">
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               for="title">
+                            Thread Title
+                        </label>
+                        <input
+                            name="title"
+                            class=
+                            "@error('title') border-red-300 @enderror appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4  leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="title"
+                            type="text"
+                            value="{{ old('title') }}"
+                            autofocus
+                            placeholder="I need help with ...">
+
+                            @error('title')
+                            <span class="text-red-500 text-xs italic" role="alert">
+                                    {{ $message }}
+                                </span>
+                            @enderror
+                    </div>
+
+                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               for="channel">
+                            Choose a Channel
+                        </label>
+   
+                      <div class="relative">
+                        <select 
+                        name="channel_id"
+                        class="@error('channel_id') border-red-300 @enderror block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="channel">
+                          <option value="">Choose One</option>
+                        @foreach ($channels as $channel)
+                            <option
+                                value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                                {{ $channel->name }}
+                            </option>
+                        @endforeach
+                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                        </div>
+                      </div>
+
+                        @error('channel_id')
+                        <span class="text-red-500 text-xs italic" role="alert">
+                                {{ $message }}
+                        </span>
+                        @enderror
+                    </div>                    
+                </div>
+                    <div class="w-full mb-4">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                               for="body">
+                            Thread Body
+                        </label>
+                        <vue-trix placeholder="Enter Your content here ..." class="@error('body') border-red-300 @enderror"></vue-trix>
+                        @error('body')
+                        <span class="text-red-500 text-xs italic" role="alert">
+                                {{ $message }}
+                        </span>
+                        @enderror
+                    </div>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Publish</button>
+
+            </form>
+        </div>
+    </div>    
+</div>
 @endsection
